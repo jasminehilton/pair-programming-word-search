b5448ac2f7a89f68e8e8c35a05ab99b509323b66
Jasmine Hilton <109497131+jasminehilton@users.noreply.github.com>
2023-07-06 21:06:59 -0700

    Update wordsearch.js

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,34 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+const transpose = function (matrix) {
+  const rows = matrix.length;
+  const cols = matrix[0].length;
 
-module.exports = wordSearch
+  const transposedMatrix = Array.from({ length: cols }, () => new Array(rows));
+
+  for (let i = 0; i < rows; i++) {
+    for (let j = 0; j < cols; j++) {
+      transposedMatrix[j][i] = matrix[i][j];
+    }
+  }
+
+  return transposedMatrix;
+};
+
+const wordSearch = (letters, word) => {
+  const horizontalJoin = letters.map((letter) => letter.join(""));
+
+  const transposedLetters = transpose(letters);
+
+  const verticalJoin = transposedLetters.map((letter) => letter.join(""));
+
+  for (let l of horizontalJoin) {
+    if (l.includes(word)) return true;
+  }
+
+  for (let l of verticalJoin) {
+    if (l.includes(word)) return true;
+  }
+
+  return false;
+};
+
+module.exports = wordSearch;
